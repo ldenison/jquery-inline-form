762f68c31a2989fbd10e20cf5df8ddd653a463ca
ldenison <ldenison@emich.edu>
2014-03-07 16:50:41 -0500

    Fixed formatting. Added comments.

--- inline-form.js
+++ inline-form.js
@@ -1,9 +1,20 @@
+/**
+ * JQuery Inline Form
+ * This allows you to place an element on a web page that will turn into 
+ * a text input when the user clicks it. Includes a submit and cancel
+ * button to either submit the form, or return the screen to its previous
+ * state, with the element showing and the form hidden. Great for allowing
+ * users to edit data anywhere when displaying some data that might already
+ * be saved in a database.
+ * @author - Luke Denison
+ * @date - March 2014
+ */
+ 
 (function( $ ) {
  
     $.fn.inlineForm = function(options) {
     	
     	var button = $(this);
-    	
     	var settings = $.extend({
     		name: "name",
     		id: null,
@@ -11,6 +22,7 @@
     		method: "post"
     	}, options);
     	
+    	//Initialize additional elements for the hidden form
     	var form = $("<form>", {
     		class: "col-xs-3",
     		id: settings.id,
@@ -30,24 +42,28 @@
     		form: "none",
     		html: "<i class='glyphicon glyphicon-remove' style='color: red'></i>"
     	});
+    	//Add the input and cancel buttons to the form
+	form.prepend(input);
+	form.append(cancel);
     	
-		form.prepend(input);
-		form.append(cancel);
-    	
+    	//Add the form after the editable element
     	$(this).after(form);
     	$(form).hide();
     	var cancel = $(form).find(".cancel");
     	
+    	//Hide the form when the cancel button is clicked and reset the value of the input
     	$(cancel).click(function () {
     		$(input).val($(button).text());
     		$(form).hide();
     		$(button).show();
     	});
     	
+    	//Display the form when the element is clicked
     	$(button).click(function() {
     		$(this).hide();
     		$(form).show();
     	});
+    	//Return the button for JQuery chaining
         return this;
     };
 }( jQuery ));
